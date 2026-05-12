82e8fb02ce8c64f0dd33419c71a70d8444b467f5
IndyCryDevil <is.rs.math@buodisha.edu.in>
2026-05-12 18:57:06 +0530

    Fixed duplicate CORS middleware

--- backend/main.py
+++ backend/main.py
@@ -21,30 +21,17 @@ from einsteinpy.symbolic import (
 )
 
 app = FastAPI()
+origins = [
+    "https://relativity-tensor-lab.vercel.app",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-app.add_middleware(
-
-    CORSMiddleware,
-
-    allow_origins=[
-        "http://localhost:3000"
-    ],
-
-    allow_credentials=True,
-
-    allow_methods=["*"],
-
-    allow_headers=["*"],
-
-)
-
 
 class TensorRequest(BaseModel):
 
