dc70bd57f436b31968fbdbcdc02100ae9952edfc
IndyCryDevil <is.rs.math@buodisha.edu.in>
2026-05-12 18:39:00 +0530

    Fixed CORS for deployment

--- backend/main.py
+++ backend/main.py
@@ -21,6 +21,13 @@ from einsteinpy.symbolic import (
 )
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.add_middleware(
 
